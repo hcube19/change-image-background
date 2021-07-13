f279153806465191b804c72f39fb7095d4ed6a5f
hcube19 <86916688+hcube19@users.noreply.github.com>
2021-07-13 16:38:12 +0530

    Add files via upload

---
+++ bgremove.py
@@ -0,0 +1,18 @@
+#import pixellib
+import pixellib
+
+import os
+
+#from pixellib we imported in the class alter_bg
+from pixellib.tune_bg import alter_bg
+
+change_bg = alter_bg(model_type = "pb")
+
+#load the deeplabv3+ model
+change_bg.load_pascalvoc_model("D:\\xception_pascalvoc.pb")
+
+#f_image_path:the image which background would be changed. (original image)
+#b_image_path: background image for original image
+#output_image_name: The new image with a changed background.
+
+change_bg.change_bg_img(f_image_path = "D:\\view2.jpg",b_image_path = "D:\\back.jpg", output_image_name="D:\\new_img.jpg")
