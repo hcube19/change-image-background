8d6e64b5561d8045d9e1c75ed2ebc43560d87c77
hcube19 <86916688+hcube19@users.noreply.github.com>
2021-07-13 16:38:51 +0530

    Update bgremove.py

--- bgremove.py
+++ bgremove.py
@@ -14,5 +14,4 @@ change_bg.load_pascalvoc_model("D:\\xception_pascalvoc.pb")
 #f_image_path:the image which background would be changed. (original image)
 #b_image_path: background image for original image
 #output_image_name: The new image with a changed background.
-
 change_bg.change_bg_img(f_image_path = "D:\\original.jpg",b_image_path = "D:\\back.jpg", output_image_name="D:\\new_img.jpg")
